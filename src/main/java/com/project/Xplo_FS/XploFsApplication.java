b97b4b58a4a03a1621fed5215aa6b2f5b1a28d4b
Justice <nkopane.mj@gmail.com>
2024-08-08 12:43:04 +0200

    first commit

---
+++ src/main/java/com/project/Xplo_FS/XploFsApplication.java
@@ -0,0 +1,13 @@
+package com.project.Xplo_FS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XploFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XploFsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Xplo_FS/XploFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Xplo_FS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XploFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
